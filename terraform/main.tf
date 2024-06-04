713fbda57736d409c87ebba93c7e870ba1c4fc58
nyilinhtut1994 <nyilinhtut1994@gmail.com>
2024-06-04 21:00:50 +0630

    fix terraform output file

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,8 +2,11 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.0"
+      version = "~> 4.0"
     }
+  }
+  backend "s3" {
+    key = "aws/ec2-deploy/terraform.tfstate"
   }
 }
 
